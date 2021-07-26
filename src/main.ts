69a373a62192c47f573a33dbad3ff8aa30825ae8
rusminanto <tyas@shipper.id>
2021-07-26 21:42:08 +0700

    adding comment for some updated code

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // adding validation pipes for all endpoints
   app.useGlobalPipes(new ValidationPipe())
 
+  // setup swagger document properties
   const swConfig = new DocumentBuilder()
   .setTitle('Test API')
   .setDescription('Documentation for Test API in my local computer')
   .setVersion('1.0')
   .build()
 
+  // register document into swagger module
   const swagDoc = SwaggerModule.createDocument(app, swConfig)
+  // bind swagger module into app
   SwaggerModule.setup('docs', app, swagDoc)
 
   await app.listen(3000);
