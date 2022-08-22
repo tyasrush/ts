b22361d84b1e361771885d3a5e3f08f4e2602ee4
kincus <user@slenthem.local>
2022-08-22 08:11:52 +0700

    adding getServerSideProps for testing

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,10 +1,17 @@
 import {useRouter} from "next/router";
 
-function Post() {
+function Post({ data }) {
   const router = useRouter()
   const {id} = router.query
 
+  console.log("data from server side: " + data)
+
   return <div><h1>Post ID: {id}</h1></div>
+}
+
+export async function getServerSideProps(context) {
+  console.log("getServerSideProps called!")
+  return { props: { data: 3 }}
 }
 
 export default Post
